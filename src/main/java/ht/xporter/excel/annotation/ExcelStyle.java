5aad04bc2aadc8458dfd05e2eb585b550619f359
Hugo Tota <hugo.tota@gmail.com>
2013-04-01 17:24:14 -0300

    initial commit

--- src/main/java/ht/xporter/excel/annotation/ExcelStyle.java
+++ src/main/java/ht/xporter/excel/annotation/ExcelStyle.java
@@ -9,9 +9,20 @@ import ht.xporter.excel.style.CellType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation for configuration of excel styles
+ * 
+ * @author hugo.tota@gmail.com
+ * @version 1.0
+ */
 @Retention(RUNTIME)
 @Target(value = FIELD)
 public @interface ExcelStyle {
+	/**
+	 * 
+	 * 
+	 * @return  
+	 */
 	CellType cellType();
 	String pattern() default "";
 	BorderFill border() default NONE;	
